002e4b6cdd991b85792f94b5cc71e9d27f13a855
Bogdan-Teodora <teodora.bogdan1@stud.ubbcluj.ro>
2023-04-02 21:53:57 +0300

    Update main.tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-export const GlobalURL="http://16.16.98.14";
+export const GlobalURL="https://lab5-carrental.netlify.app/";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
