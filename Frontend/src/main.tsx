fc4ba70568cf24bece5b32a54f5c190720a38809
Bogdan-Teodora <teodora.bogdan1@stud.ubbcluj.ro>
2023-04-02 22:23:29 +0300

    .

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,11 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
 export const GlobalURL="https://lab5-carrental.netlify.app/";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
