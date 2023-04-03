1e765b7b77522e69376eb64e511ea5d1e500ee79
Bogdan-Teodora <teodora.bogdan1@stud.ubbcluj.ro>
2023-04-03 10:22:01 +0300

    .

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-export const GlobalURL="https://lab5-carrental.netlify.app/";
+export const GlobalURL="http://13.53.139.219";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+//TODO : intreb pe chatcpt ca daca am codul in vs code pt site si eu am facut deplyment in netlify 
+//cum pot sa fac sa imi apara cand dau run din vs code site.ul de la netlify si nu localhost:number       
