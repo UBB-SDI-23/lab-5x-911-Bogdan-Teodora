fc88cb7d7cbe5cb0489d2be532cf6fac4838ebfa
Bogdan-Teodora <teodora.bogdan1@stud.ubbcluj.ro>
2023-04-03 10:46:09 +0300

    .

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-export const GlobalURL="http://13.53.139.219";
+export const GlobalURL="https://13.53.139.219";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
