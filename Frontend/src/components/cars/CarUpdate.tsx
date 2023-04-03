b1cc592716a0b46deb5b783d6d9b50eecac9b65c
Bogdan-Teodora <teodora.bogdan1@stud.ubbcluj.ro>
2023-04-03 14:41:01 +0300

    Create CarUpdate.tsx

---
+++ Frontend/src/components/cars/CarUpdate.tsx
@@ -0,0 +1,105 @@
+import { Button, Card, CardActions, CardContent, IconButton, TextField } from "@mui/material";
+import { Container } from "@mui/system";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import axios from "axios";
+import { Car } from "../../models/Car";
+import { GlobalURL } from "../../main";
+import { BACKEND_API_URL } from "../../constants";
+
+export const carUpdate = () => {
+    const { id } = useParams();
+	const navigate = useNavigate();
+    const [loading, setLoading] = useState(true)
+	const [car, setCar] = useState<Car>({
+        id: 0,
+        model: "",
+        brand: "",
+        color: "",
+        year_manufacture: 0,
+        nrkilometers: 1,
+
+	});
+
+    useEffect(() => {
+		const fetchcar = async () => {
+			const response = await fetch(`${BACKEND_API_URL}/car/${id}/edit`);
+			const fetchedcar = await response.json();
+			setCar(fetchedcar);
+			setLoading(false);
+		};
+		fetchcar();
+	}, [id]);
+
+	const updatecar = async (event: { preventDefault: () => void }) => {
+		event.preventDefault();
+		try {
+			await axios.put(`/api/cars/${id}/edit`, car);
+			navigate("/cars");
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<Container>
+			<Card>
+				<CardContent>
+					<IconButton component={Link} sx={{ mr: 3 }} to={`/cars`}>
+						<ArrowBackIcon />
+					</IconButton>{" "}
+					<form onSubmit={updatecar}>
+						<TextField
+							id="model"
+							label="Model"
+							variant="outlined"
+							fullWidth
+							sx={{ mb: 2 }}
+							onChange={(event) => setCar({ ...car, model: event.target.value })}
+						/>
+						<TextField
+							id="brand"
+							label="Brand"
+							variant="outlined"
+							fullWidth
+							sx={{ mb: 2 }}
+							onChange={(event) => setCar({ ...car, brand: event.target.value })}
+						/>
+                        <TextField
+                                id="color"
+                                label="Color"
+                                variant="outlined"
+                                fullWidth
+                                sx={{ mb: 2 }}
+                                onChange={(event) => setCar({ ...car, color: event.target.value })}
+                            />
+
+                        <TextField
+							id="year_manufacture"
+							label="Year of manufacture"
+							variant="outlined"
+							fullWidth
+							sx={{ mb: 2 }}
+							onChange={(event) => setCar({ ...car, year_manufacture: parseInt(event.target.value) })}
+						/>
+                        <TextField
+							id="nrkilometers"
+							label="Number Kilometers"
+							variant="outlined"
+							fullWidth
+							sx={{ mb: 2 }}
+							onChange={(event) => setCar({ ...car, nrkilometers: parseInt(event.target.value) })}
+						/>
+
+                        
+						<Button type="submit">Update car details</Button>
+					</form>
+				</CardContent>
+				<CardActions></CardActions>
+			</Card>
+		</Container>
+	);
+};
