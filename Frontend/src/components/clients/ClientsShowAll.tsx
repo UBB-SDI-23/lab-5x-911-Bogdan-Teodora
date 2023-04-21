855e932f1a8583e0236db99a842fe17f779cf4e1
Bogdan-Teodora <teodora.bogdan1@stud.ubbcluj.ro>
2023-04-21 15:09:36 +0300

    Update ClientsShowAll.tsx

--- Frontend/src/components/clients/ClientsShowAll.tsx
+++ Frontend/src/components/clients/ClientsShowAll.tsx
@@ -25,177 +25,131 @@ import AddIcon from "@mui/icons-material/Add";
 import { GlobalURL } from "../../main";
 import { BACKEND_API_URL } from "../../constants";
 import { Clients } from "../../models/Client";
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 
-const PAGE_SIZE = 100;
+
 export const ClientsShowAll = () => {
-    const [loading, setLoading] = useState(false);
-    const [clients, setClients] = useState<Clients[]>([]);
-    const [pageSize, setPageSize] = useState(100);
-    const [totalClients, setTotalClients] =useState(0)
-    const [currentPage, setCurrentPage]=useState(0)
-  
+  const[loading, setLoading] = useState(true);
+  const[clients, setClients] = useState<Clients[]>([]);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const crt = (page - 1) * pageSize + 1;
+
+  const fetchClients = async () => {
+      setLoading(true);
+      const response = await fetch(
+        `${BACKEND_API_URL}/clients/paged?page=${page}&size=${pageSize}`
+      );
+      const { count, next, previous, results } = await response.json();
+      setClients(results);
+      setLoading(false);
+    };
   
     useEffect(() => {
-      setLoading(true);
-  
-      const fetchClients = () => {
-        fetch(`${BACKEND_API_URL}/clients/paged?page=${currentPage}&size=${pageSize}`)
-          .then((response) => response.json())
-          .then((data) => {
-            setTotalClients(100000);
-            setClients(data);
-            setLoading(false);
-          });
-        
-      };
       fetchClients();
-    }, [currentPage, pageSize]);
-  
-    
-    const handlePreviousPage = () => {
-      if(currentPage>0)
-      {
-        setCurrentPage(currentPage-1);
-      }
-    };
-  
-    const handleNextPage = () => {
-      setCurrentPage(currentPage+1);
-    };
+    }, [page]);
 
-    const sortClients = () => {
-        const sortedClients = [...clients].sort((a: Clients, b: Clients) => {
-            if (a.lname < b.lname) {
-                return -1;
-            }
-            if (a.lname > b.lname) {
-                return 1;
-            }
-            return 0;
-        })
-        console.log(sortedClients);
-        setClients(sortedClients);
-    }
+  const sortClients = () => {
+      const sortedClients = [...clients].sort((a: Clients, b: Clients) => {
+          if (a.lname < b.lname) {
+              return -1;
+          }
+          if (a.lname > b.lname) {
+              return 1;
+          }
+          return 0;
+      })
+      console.log(sortedClients);
+      setClients(sortedClients);
+  }
   
-    return (
-      <Container>
-        <h1>All clients</h1>
-  
-        {loading && <CircularProgress />}
-        {!loading && clients.length === 0 && <p>No clients found</p>}
-        {!loading && (
-          <div style={{display:'flex', alignItems:'center'}}>
-            <IconButton component={Link} sx={{marginRight: 0 }} to={`/clients/add`}>
+  return (
+  <Container>
+      <h1 style={{marginTop:"65px"}}>All Clients</h1>
+
+      {loading && <CircularProgress />}
+
+      {!loading && clients.length == 0 && <div>No clients found</div>}
+
+      {!loading && (
+          <IconButton component={Link} sx={{ mr: 3 }} to={`/clients/add`}>
               <Tooltip title="Add a new client" arrow>
-                <AddIcon color="primary" />
+                  <PersonAddAlt1Icon style={{color:"whitesmoke", fontSize:"50px"}} />
               </Tooltip>
-            </IconButton>
-          </div>
-        )}
-  
-        {!loading && (
-              <div style ={{display: "flex", alignItems:"center"}}>
-                 
-                  <Button
-                    sx={{color:"black"}}
-                    disabled={currentPage===0}
-                    onClick={handlePreviousPage}>
-                      Previous Page
-                  </Button>
-                  <Button
-                   sx={{color:"black"}} onClick={handleNextPage}>
-                    Next Page
-                   </Button>
-  
-                   <Box mx={2} display="flex" alignItems="center">
-                    Page {currentPage+1} of {Math.ceil(totalClients/pageSize)}
-                   </Box>
-              </div>
-              )}
-        {!loading && (
-                <Button sx={{color:"black"}} onClick={sortClients} >
-                    Sort clients by last name
-                </Button>
-            )}
-  
-        {!loading && clients.length > 0 && (
+          </IconButton>
+
+      )}
+
+      {!loading && (
+              <Button sx={{color:"black"}} onClick={sortClients} >
+                  Sort clients
+              </Button>
+          )}
+
+      {!loading && clients.length > 0 && (
+        <>
           <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                <TableCell>#</TableCell>IdCar
-                  <TableCell align="right">Phone Number</TableCell>
-                  <TableCell align="right">Email Address</TableCell>
-                  <TableCell align="center">Date Of Birth</TableCell>
-                  <TableCell align="center">Address Id</TableCell>
-                  <TableCell align="right">First Name</TableCell>
-                  <TableCell align="right">Last Name</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-              {clients.map((client:Clients, index) => (
-                    <TableRow key={index}>
-                      
-                      <TableCell component="th" scope="row">
-                        {index + 1}
-                      </TableCell>
-                      <TableCell component="th" scope="row">
-                        <Link to={`/clients/${client.idClient}/details`} title="View client details">
-                          {client.idClient}
-                        </Link>
-                    </TableCell>
-  
-                    <TableCell align="right">{client.phoneNR}</TableCell>
-                    <TableCell align="right">{client.email_address}</TableCell>
-                    <TableCell align="right">{client.dateOfBirth}</TableCell>
-                    <TableCell align="right">{client.addressID}</TableCell>
-                    <TableCell align="right">{client.fname}</TableCell>
-                    <TableCell align="right">{client.lname}</TableCell>
-                    
-                    <TableCell align="right">
-                    <IconButton
-                          component={Link}
-                          sx={{ mr: 3 }}
-                          to={`/clients/${client.idClient}/details`}>
-                          <Tooltip title="View client details" arrow>
-                            <ReadMoreIcon color="primary" />
-                          </Tooltip>
-                        </IconButton>
-  
-                      <IconButton
-                        component={Link}
-                        sx={{ mr: 3 }}
-                        to={`/clients/${client.idClient}/edit`}
-                        title="Edit client details"
-                      >
-                        <EditIcon />
-                      </IconButton>
-  
-                      <IconButton
-                        component={Link}
-                        sx={{ mr: 3 }}
-                        to={`/clients/${client.idClient}/delete`}
-                        title="Delete client"
-                      >
-                        <DeleteForeverIcon sx={{ color: "red" }} />
-                      </IconButton>
-                    </TableCell>
-                  </TableRow>
-                ))}
+              <Table sx={{ minWidth: 800 }} aria-label="simple table" style={{backgroundColor:"whitesmoke"}}>
+                  <TableHead>
+                      <TableRow>
+                          <TableCell align="center" style={{color:"#2471A3", fontWeight:'bold'}}>Phone number</TableCell>
+                          <TableCell align="center" style={{color:"#2471A3", fontWeight: 'bold'}}>Email address</TableCell>
+                          <TableCell align="center" style={{color:"#2471A3", fontWeight: 'bold'}}>Date of birth</TableCell>
+                          <TableCell align="center" style={{color:"#2471A3", fontWeight: 'bold'}}>Address Id</TableCell>
+                          <TableCell align="center" style={{color:"#2471A3", fontWeight: 'bold'}}>First name</TableCell>
+                          <TableCell align="center" style={{color:"#2471A3", fontWeight: 'bold'}}>Last name</TableCell>
+                          <TableCell align="center" style={{color:"#2471A3", fontWeight: 'bold'}}>Operations</TableCell>
+
+                      </TableRow>
+                  </TableHead>
+                  <TableBody>
+                      {clients.map((client:Clients, index) => (
+                          <TableRow key={client.idClient}>
+                              <TableCell component="th" scope="row">
+                                  {index + crt}
+                              <TableCell component="th" scope="row">
+                                  <Link to={`/clients/${client.idClient}/details`} title="View clients details">
+                                      {"view details"}
+                                      
+                                  </Link>
+
+                              </TableCell>
+                              </TableCell>
+                              <TableCell align="center">{client.phoneNR}</TableCell>
+                              <TableCell align="center">{client.email_address}</TableCell>
+                              <TableCell align="center">{client.dateOfBirth}</TableCell>
+                              <TableCell align="center">{client.addressID}</TableCell>
+                              <TableCell align="center">{client.fname}</TableCell>
+                              <TableCell align="center">{client.lname}</TableCell>
+                              <TableCell align="center">
+
+                  <IconButton component={Link} sx={{ mr: 3 }} to={`/clients/${client.idClient}/edit`}>
+                    <EditIcon />
+                  </IconButton>
+
+                  <IconButton component={Link} sx={{ mr: 3 }} to={`/clients/${client.idClient}/delete`}>
+                    <DeleteForeverIcon sx={{ color: "red" }} />
+                  </IconButton>
+                </TableCell>
+                          </TableRow>
+                      ))}
               </TableBody>
-            </Table>
+              </Table>
           </TableContainer>
-        )}
-        <Button
-            sx={{color:"black"}}
-            disabled={currentPage===0}
-            onClick={handlePreviousPage}>
-              Previous Page
-            </Button>
-  
-          <Button sx={{color:"black"}} onClick={handleNextPage}>
-            Next Page
+          <Button style={{color:"whitesmoke"}} disabled={page === 1} onClick={() => setPage(page - 1)}>
+          Previous Page
           </Button>
-      </Container>
-    );
-  };
+
+          <Button style={{color:"whitesmoke"}}
+          disabled={clients.length < pageSize}
+          onClick={() => setPage(page + 1)}
+          >
+          Next Page
+          </Button>
+        </>
+      )
+      }
+  </Container>
+      
+  );       
+};
