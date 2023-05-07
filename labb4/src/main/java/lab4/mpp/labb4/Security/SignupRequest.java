d2f4c97de298211e4beb473d544af8742be424e1
Bogdan-Teodora <teodora.bogdan1@stud.ubbcluj.ro>
2023-05-07 16:24:36 +0300

    .

---
+++ labb4/src/main/java/lab4/mpp/labb4/Security/SignupRequest.java
@@ -0,0 +1,42 @@
+//package lab4.mpp.labb4.Security;
+//
+////import javax.persistence.Column;
+////import javax.validation.constraints.NotBlank;
+//
+//import jakarta.persistence.Column;
+//import jakarta.validation.constraints.NotBlank;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//import java.util.Date;
+//
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class SignupRequest {
+//
+//    @NotBlank
+//    private String username;
+//
+//    @NotBlank
+//    private String password;
+//
+//    @Column
+//    private String name;
+//
+//    @Column
+//    private String gender;
+//
+//    @Column
+//    private String bio;
+//
+//    @Column
+//    private String location;
+//
+//    @Column
+//    private Date birthdate;
+//
+//}
